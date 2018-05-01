b6b418053db119ba89cf9a205a001fe28e33e76f
Kate Stearns <katestearns@suplari.com>
2018-05-01 08:24:04 -0700

    update routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const pg = require('pg');
+const path = require('path');
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/shopping-cart';
+
 // Cart Routes
 // quantity | item
 // ---------+-------
