8bda628be0c962cbf4901dbd03abb34557f414e7
Kate Stearns <katestearns@suplari.com>
2018-05-01 14:22:14 -0700

    patrick part 1

--- routes/index.js
+++ routes/index.js
@@ -56,5 +56,128 @@ router.get('/api/v1/carts', (req, res, next) => {
    });
  })
 
+ router.put('/api/v1/carts/:id', (req, res, next) => {
+    const results = [];
+    const data = {quantity: req.body.quantity, item: req.body.item};
+
+    pg.connect(connectionString, (err, client, done) => {
+        if(err) {
+            done();
+            console.log(err);
+            return res.status(500).json({success : false, data: err});
+        }
+
+        const id = req.params.id;
+        const query = client.query('UPDATE cart SET quantity=($1), item=($2) WHERE id=($3)', [data.quantity, data.item, id]);
+        query.on('row', (row) => {
+            results.push(row);
+        });
+
+        query.on('end', () => {
+            done();
+            return res.json(results);
+        });
+    });
+ })
+
+ router.delete('/api/v1/carts/:id', (req, res, next) => {
+     const results = [];
+
+     pg.connect(connectionString, (err, client, done) => {
+         if (err) {
+             done();
+             console.log(err);
+             return res.status(500).json({success: false, data: err});
+         }
+
+         const id = req.params.id;
+         const query = client.query('DELETE FROM cart WHERE id=($1)', [id]);
+         query.on('row', (row) => {
+             results.push(row);
+         });
+
+         query.on('end', ()=> {
+             done();
+             return res.json(results);
+         })
+     })
+ })
+
+ // products Routes
+
+ router.get('/api/v1/products', (req, res, next)=> {
+     const results = [];
+
+     pg.connect(connectionString, (err, client, done) => {
+         if (err) {
+             done();
+             console.log(err);
+             return res.status(500).json({success: false, data: err});
+         }
+
+         const query = client.query('SELECT * FROM products ORDER BY price DESC;');
+         query.on('row', (row)=> {
+             results.push(row);
+         })
+
+         query.on('end', () => {
+             done();
+             return res.json(results);
+         });
+     });
+ })
+
+ router.post('/api/v1/products', (req, res, next) => {
+     const results = [];
+     const data = {name: req.body.name, description: req.body.description, price: req.body.price, imgurl: req.body.imgurl};
+
+     pg.connect(connectionString, (err, client, done) => {
+         if (err) {
+             done();
+             console.log(err);
+             return res.status(500).json({success: false, data: err});
+         }
+
+         client.query('INSERT INTO products(name, description, price, imgurl) values($1, $2, $3, $4)', [data.name, data.description, data.price, data.imgurl]);
+
+         const query = client.query('SELECT * FROM products ORDER BY price DESC');
+         query.on('row', (row) => {
+             results.push(row);
+         });
+
+         query.on('end', () => {
+             done();
+             return res.json(results);
+         });
+     });
+ })
+
+ router.put('/api/v1/products/:id', (req, res, next) => {
+     const results = [];
+     const data = {name: req.body.name, description: req.body.description, price: req.body.price, imgurl: req.body.imgurl};
+
+     pg.connect(connectionString, (err, client, done) => {
+         if (err) {
+             done();
+             console.log(err);
+             return res.status(500).json({success: false, data: err});
+         }
+
+         const id = req.params.id
+         client.query('UPDATE products SET name=($1), description=($2), price=($3), imgurl=($4) WHERE id=($5)', [data.name, data.description, data.price, data.imgurl, id])
+
+         const query = client.query('SELECT * FROM products ORDER BY price DESC');
+         query.on('row', (row) => {
+             results.push(row);
+         });
+
+         query.on('end', () => {
+             done();
+             return res.json(results);
+         });
+
+     })
+ })
+
 
 module.exports = router;
